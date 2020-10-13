71e956863a8de43ff710d0ea8030e81d0a060060
shweta hatwar <shweta@briot.in>
2020-10-13 12:18:10 +0530

    Login Screen UI created

---
+++ app/src/main/java/com/zebra/rfid/demo/sdksample/view/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.zebra.rfid.demo.sdksample.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.zebra.rfid.demo.sdksample.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
